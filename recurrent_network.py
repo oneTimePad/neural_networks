626cc31b29808c9918f6409ff4f7ec4bdc90bd17
Dylan Herman <dah242@scarletmail.rutgers.edu>
2017-05-27 13:52:52 -0400

    finished RNN backward pass

--- recurrent_network.py
+++ recurrent_network.py
@@ -58,6 +58,9 @@ class RNNetwork(object):
         return [self.language[np.argmax(o)] for o in self.outputs]
 
     def backward_pass(self,inputs,targets):
+        """
+        performs backprop in time for multi-layer RNN
+        """
         #go backwards in time
         #gradient for the output weights
             self.weights_hidden=[first_W]+[np.random.randn(hl_size,2*hl_size) for i in range(0,num_hl-1)]
@@ -66,7 +69,7 @@ class RNNetwork(object):
         dWh = [ np.zeros_like(w) for w in self.weights_hidden]
         dbh = [ np.zeros_like(b) for b in self.biases_hidden ]
         #used for backward pass (left) columns are vertical backward passes
-        dhback = [np.zeros_like(w) for w in self.weights_hidden]
+        dhback = [np.zeros_like(w) for w in self.biases_hidden]
         for i in reversed(range(0,len(inputs))):
             #dWy can be calculate very efficiently , just done this way to make it explicit
             #target vector for input i
@@ -81,23 +84,30 @@ class RNNetwork(object):
             half_delta =np.dot(self.weights_output.transpose(),dEy)
             #last layer hidden state dE_i/dz_ij
             # dE_ij/dz_ij = dE_ij/d_hji *dh_ij/dz_ij (unit error, same as feedforward NN)
-            delta = half_delta*(1-self.hidden_states[i][-1]**2)
+            delta = half_delta*(1.0-self.hidden_states[i][-1]**2)
+            #dh = dE_i/dz_ij  + dE_i+1/dz_ij + ... dE_len(input)-1/dz_ij
+            dh  = delta + dhback[-1]*(1.0-self.hidden_states[i][-1]**2)
             #fetch states for this timestep at each layer
             hsi = self.hidden_states[i]
             #highest layer hidden bias
-            dbh[-1]+=delta
+            dbh[-1]+=dh
             #Wh = [Wh Wx] thus dWh = [delta (outer) hi-1,j delta(outer)h[i][j-1] ]
-            dWh[-1]+=np.concatenate((np.dot(delta,self.hidden_states[i-1][-1].transpose()),\
+            dWh[-1]+=np.concatenate((np.dot(dh,self.hidden_states[i-1][-1].transpose()),\
                     np.dot(delta,self.hidden_states[i][-2]).transpose()),axis=1)
-
-            for j in range(0,len(self.weights_hidden)):
+            #backpass to the left the cumuative sum  of dE_i's (make them all with respec to the next hidden layer to the left)
+            #Whh_T*dh
+            dhback[-1] = np.dot(self.weights_hidden[-1][:,:self.hl_size].transpose(),dh)
+            #need to repeat this for all lower layers
+            for j in range(2,len(self.weights_hidden)+1):
                 #never need to back into input so its ok
                 #fetch Whx(x means the input to the state, this is for multi-layer RNN)
+                #backpass downwards
                 #back into prev layer state (dE_i/dz_ij (i:=timestep,j:=layer))
+                #Whx_T*delta (pass delta down layers)
                 delta = np.dot(self.weights_hidden[-j+1][:,self.hl_size:].transpose,delta)*(1.0-hsi[-j]*hsi[-j])
-
-
-                hsj = hsi[j]
-                dtanh = 1.0-hsj*hsj
-                dWh[j]+=delta*dtanh
-            #HIDDEN weights/biases
+                #cumulative dE_i's with respect to the zij layer
+                dh = delta + dhback[-j]*(1.0-self.hsi[-j]**2)
+                dbh[-j]+=dh
+                dWh[-j]+=np.concatenate((np.dot(dh,self.hidden_states[i-1][-j].transpose()),\
+                        np.dot(delta,self.hsi[-j-1]).transpose()),axis=1)
+                dhback[-j] =np.dot(self.weights_hidden[-j][:,self.hl_size:])
